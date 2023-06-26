56905433482dfc5ee4783f426219287b6445661b
houtianyu <1966245176@qq.com>
2023-06-26 22:10:30 +0800

    修改了活动进程管理页面

--- src/pages/AdminPage/SiteSettingsPage.js
+++ src/pages/AdminPage/SiteSettingsPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PageTitle from "../../components/PageTitle";
 
 const SiteSettingsPage = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
